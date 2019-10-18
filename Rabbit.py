14c1c5756f8cbe50c528df2e9c6c56f7175ce4f8
medalyDragon <55749048+medalyDragon@users.noreply.github.com>
2019-10-18 20:42:40 +0000

    Update and rename DDOS.py to Rabbit.py

---
+++ Rabbit.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python 
+
+
+
+	
+print ('''
+\033[1;31;40m
+       ____       _     _     _ _
+      |  _ \ __ _| |__ | |__ (_) |_
+      | |_) / _` | '_ \| '_ \| | __|
+      |  _ < (_| | |_) | |_) | | |_
+      |_| \_\__,_|_.__/|_.__/|_|\__|	     
+
+
+  ''')
+print ("\033[1;32;40m by Mohamed Aly Sidi Mohamed")
+print ("\033[1;33;40m Black Hat Dragon")
+print ("\033[1;34;40m Mauritanian Hackers Organization")
+
+target_IP = input("\033[1;31;40mEnter IP of Target: ")
+
+
+
+ 
